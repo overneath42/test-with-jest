e4d415f198f8d934417ae979f69a47b0b0827bba
Justin Toon <jtoon@missiondata.com>
2018-03-30 10:18:28 -0400

    Add a simple spy demonstration

--- app/javascript/components/__tests__/Page.jsx
+++ app/javascript/components/__tests__/Page.jsx
@@ -5,7 +5,7 @@ import randomWords from 'files/random-words';
 import { Page } from '../Page';
 import { PageHeader } from '../PageHeader';
 
-let title, subtitle;
+let title, subtitle, spy;
 
 beforeEach(() => {
   title = randomWords(3);
@@ -31,7 +31,7 @@ describe('<Page />', () => {
   it('only renders an instance of <PageHeader /> if a title is given', () => {
     /*
       `shallow` mounts the current component and any children, but only calls
-      a limited portion of the lifecycle. Typically, it is suitable to just call `mount`.
+      a limited portion of the lifecycle. Typically, it is okay to just call `mount`.
     */
     const page = shallow(<Page />);
 
@@ -40,13 +40,33 @@ describe('<Page />', () => {
 
   it('properly tracks if the page is marked as read', () => {
     /*
-      Internal state can be tracked, and events can be simulated.
+      Internal methods can be tracked by spying on the method within the component
+      prototype. It is important to remember that the spy must be set **before**
+      the component is mounted.
+     */
+    spy = jest.spyOn(Page.prototype, 'handleMarkAsRead');
+    /*
+      Internal state can be tracked when using `shallow` or `mount`.
     */
     const page = shallow(<Page title={title} subtitle={subtitle} />);
-
     expect(page.state().isRead).toBe(false);
+
+    /*
+      Events can be simulated on specific events by querying for them within
+      the mounted component.
+    */
     page.find('button').simulate('click');
+
+    expect(spy).toBeCalled();
     expect(page.state().isRead).toBe(true);
+
+    /*
+      It's generally good practice to reset your spy at the end of your test.
+      Alternately you can also call `jest.resetAllMocks()` a/o
+      `jest.restoreAllMocks()` within an `afterEach` block.
+    */
+    spy.mockReset();
+    spy.mockRestore();
   });
 
   it('accepts children', () => {
